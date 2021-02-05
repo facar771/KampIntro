adf837ba524406cc5f63cf7380285a9075aecef6
Ferhat AÇAR <facar7711@gmail.com>
2021-02-05 12:26:00 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs a = new Kurs();
+            Kurs b = new Kurs();
+            Kurs c = new Kurs();
+
+            a.Isim = "Ferhat";
+            a.Sehir = "Kastamonu";
+            a.Yas = 21;
+
+            b.Isim = "qewqweqwe";
+            b.Sehir = "zcxzxczcx";
+            b.Yas = 12;
+
+            c.Isim = "asdasdas";
+            c.Sehir = "vvbnvbnnbv";
+            c.Yas = 212;
+
+            Console.WriteLine(a.Isim);
+
+            Kurs[] Kisiler = new Kurs[] {a,b,c};
+
+            foreach (var d in Kisiler)
+            {
+                Console.WriteLine(d.Isim);
+                Console.WriteLine(d.Sehir);
+                Console.WriteLine(d.Yas);
+                Console.WriteLine("----------------");
+            }
+
+        }
+    }
+
+    class Kurs
+    {
+        public string Isim { get; set; }
+        public string Sehir { get; set; }
+        public int Yas { get; set; }
+    }
+}
---
+++ Methodlar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Methodlar
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Urun meyve1 = new Urun();
+            meyve1.Adi = "Elma";
+            Urun meyve2 = new Urun();
+            meyve2.Adi = "dfssdfa";
+            Urun meyve3 = new Urun();
+            meyve3.Adi = "sdfsdfa";
+            Urun meyve4 = new Urun();
+            meyve4.Adi = "Edfssdfsma";
+
+            Urun[] dizi = new Urun[] { meyve1, meyve2, meyve3, meyve4 };
+
+            foreach (Urun diziler in dizi)
+            {
+                Console.WriteLine(diziler.Adi);
+            }
+
+
+        }
+    }
+}
