d4661e032a2231bf869ec70bcd57434748e6c704
facar771 <59576219+facar771@users.noreply.github.com>
2021-05-13 00:29:05 +0300

    DorduncuGunOdev5
    DorduncuGunOdev5

---
+++ DorduncuGunOdev5/MyDictionary/MyDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] words;
+        public MyDictionary()
+        {
+            words = new T[0];
+        }
+        public void Add(T word)
+        {
+            T[] tempArray = words;
+            words = new T[words.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                words[i] = tempArray[i];
+            }
+            words[words.Length-1] = word;
+        }
+        public int Length
+        {
+            get { return words.Length; }
+        }
+    } 
+}
---
+++ DorduncuGunOdev5/MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> words =new MyDictionary<string> ();
+
+            words.Add("Ferhat");
+
+            Console.WriteLine(words.Length);
+
+            words.Add("Sumeyye");
+
+            Console.WriteLine(words.Length);
+        }
+    }
+}
